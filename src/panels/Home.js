4a463f7acf75e54718d8f4e5529cf7dc4ff7dc93
PolinaFam <45046398+PolinaFam@users.noreply.github.com>
2019-12-14 15:56:33 +0700

    addHead

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,47 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
-import Div from '@vkontakte/vkui/dist/components/Div/Div';
-import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import { HeaderButton } from '@vkontakte/vkui';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
 
-const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		{fetchedUser &&
-		<Group title="User Data Fetched with VK Connect">
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
+class Home extends React.Component {
+	
+	addBut () {
+		if (this.props.back) {
+			return (<HeaderButton><Icon24Back/></HeaderButton>)
+		}
+		return;
+	}
 
-		<Group title="Navigation Example">
-			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
-				</Button>
-			</Div>
-		</Group>
-	</Panel>
-);
-
-Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
-};
+	render(){
+		return(
+		<Panel id={this.props.id}>
+			<PanelHeader left={this.addBut()}>
+				VKarma
+			</PanelHeader>
+		</Panel>
+		)
+	}
+}
 
 export default Home;
