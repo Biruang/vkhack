5b1a0454b3e7d07b65beaeb3cfcea2d0deb51112
PolinaFam <45046398+PolinaFam@users.noreply.github.com>
2019-12-14 18:54:39 +0700

    list-friends

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import { HeaderButton } from '@vkontakte/vkui';
-import Icon24Back from '@vkontakte/icons/dist/24/back';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Profile from "../components/profile";
+import ListFriends from "../components/listFriends";
+
+const arr = [
+	{
+		id:1928903,
+		first_name:'Pupkin',
+		last_name:'Zalupkin',
+		photo:'https://pp.userapi.com/c845121/v845121950/63c02/4hP61FL56YM.jpg?ava=1',
+		karma:400
+	},
+	{
+		id:1890390,
+		first_name:'Pupkin',
+		last_name:'Zalupkin',
+		photo:'https://pp.userapi.com/c845121/v845121950/63c02/4hP61FL56YM.jpg?ava=1',
+		karma:400
+	},
+	{
+		id:2389324,
+		first_name:'Pupkin',
+		last_name:'Zalupkin',
+		photo:'https://pp.userapi.com/c845121/v845121950/63c02/4hP61FL56YM.jpg?ava=1',
+		karma:400
+	},
+]
 
 class Home extends React.Component {
-	
-	addBut () {
-		if (this.props.back) {
-			return (<HeaderButton><Icon24Back/></HeaderButton>)
-		}
-		return;
-	}
 
 	render(){
 		return(
       <React.Fragment>
         <Panel id={this.props.id}>
-          <PanelHeader left={this.addBut()}>
+          <PanelHeader>
             VKarma
           </PanelHeader>
           <Group>
@@ -28,6 +44,9 @@ class Home extends React.Component {
 				      <Profile carma={2} newDealsCount={2} go={this.props.go}/>
 			      </Div>
 		      </Group>
+			  <Group title="Рейтинг друзей">
+			  	<ListFriends arrayUsers={arr}/>
+			</Group>
         </Panel>
       </React.Fragment>
 		)
