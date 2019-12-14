a60105e7e8a8187f2897bcb639a81c9669c73d4d
PolinaFam <45046398+PolinaFam@users.noreply.github.com>
2019-12-14 16:55:14 +0700

    AddForm

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import connect from '@vkontakte/vk-connect';
 import View from '@vkontakte/vkui/dist/components/View/View';
 import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
-
+import FormDeal from './FormDeal';
 import Home from './panels/Home';
-import Persik from './panels/Persik';
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
@@ -35,6 +34,7 @@ const App = () => {
 	return (
 		<View activePanel={activePanel} popout={popout}>
 			<Home id='home' fetchedUser={fetchedUser} go={go} back={false}/>
+			<FormDeal id="formDeal" go={go}/>
 		</View>
 	);
 }
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -3,6 +3,7 @@ import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import { HeaderButton } from '@vkontakte/vkui';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
 
 class Home extends React.Component {
 	
@@ -19,6 +20,9 @@ class Home extends React.Component {
 			<PanelHeader left={this.addBut()}>
 				VKarma
 			</PanelHeader>
+			<Button size="xl" level="2" onClick={this.props.go} data-to="formDeal">
+					addDeal
+				</Button>
 		</Panel>
 		)
 	}
